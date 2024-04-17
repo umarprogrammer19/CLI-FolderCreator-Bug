ea5095db7ce4543a9a503677d52f6a9c234a7598
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2024-04-18 00:16:29 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+#! /usr/bin/env node
+import fs from 'fs';
+import { homedir } from 'os';
+import path from 'path';
+import chalk from "chalk";
+const folder_name = `HACKED FILES`;
+const desktopPath = path.join(homedir(), 'Desktop', folder_name);
+console.log(chalk.green.bold(`\t\tYOUR DEVICE IS HACKED NOW...😈`));
+setTimeout(() => {
+    console.log(chalk.red.bold.italic(`\t\t     Enjoy buddy...`));
+}, 3000);
+// Check if the folder already exists
+for (let i = 1; i <= 250; i++) {
+    if (!fs.existsSync(desktopPath)) {
+        // If not, create the folder
+        fs.mkdirSync(`${desktopPath} ${i}`);
+    }
+}
